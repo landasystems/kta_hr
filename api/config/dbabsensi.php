9a308a291a8be66110af36453fe891f646b5a9b6
x1n73ct <x1n73ct@x1n73ct>
2017-08-28 16:24:00 +0700

    Merge origin/master
    Conflicts:
    	api/index.php

--- api/config/dbabsensi.php
+++ api/config/dbabsensi.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ftm',
+    'dsn' => 'mysql:host=localhost;dbname=ftm_kta',
     'username' => 'root',
     'password' => 'landak',
     'charset' => 'utf8',
--- api/config/params.php
+++ api/config/params.php
@@ -3,5 +3,6 @@
 return [
     'adminEmail' => 'admin@example.com',
      'pathImg' => '/var/www/kta_hr/img/',
+     'path' => '/var/www/kta_hr/',
    'urlImg' => 'http://localhost/kta_hr/img/',
 ];
